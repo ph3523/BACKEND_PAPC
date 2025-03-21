c460d856e352f6a5190647f9c9f68f9b03999119
EldFranklin <8wesleyfranklin@gmail.com>
2025-03-21 13:46:38 -0300

    Teste de integração com o front, cadastro paciente funcionando

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors'); 
+
 const userRoutes = require('./routes/userRoutes');
 const pacienteRoutes = require('./routes/pacientesRoutes');
+
 const app = express();
 
+// Habilita CORS para permitir requisições do front-end
+app.use(cors());
+// Permite JSON no body das requisições
 app.use(express.json());
+
 app.use('/usuarios', userRoutes);
 app.use('/pacientes', pacienteRoutes);
 
