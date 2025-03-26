7e25f8bedfb0ec094315bad59c486d5a89a25a2f
EldFranklin <8wesleyfranklin@gmail.com>
2025-03-26 12:20:47 -0300

    ajustes para depoimentos sem login

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ const grupoApoioRoutes = require('./routes/grupoApoioRoutes');
 
 const app = express();
 
-// Habilita CORS para permitir requisições do front-end
+//CORS para permitir requisições do front-end
 app.use(cors());
-// Permite JSON no body das requisições
+//Permite JSON no body das requisições
 app.use(express.json());
 
 app.use('/usuarios', userRoutes);
