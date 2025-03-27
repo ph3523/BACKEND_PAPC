124786e7f8e9a379ca63880d0221db2f62c70505
EldFranklin <8wesleyfranklin@gmail.com>
2025-03-27 12:30:11 -0300

    Merge branch 'main' of https://github.com/ph3523/BACKEND_PAPC into wes

---
+++ src/controllers/authController.js
@@ -0,0 +1,65 @@
+const userModel = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+    console.log("Dados Recebidos: ", req.body);
+
+    try {
+        const {nome_usuario, email, senha, tipo} = req.body;
+
+        if (!nome_usuario || !email || !senha || !tipo) {
+            return res.status(400).send({error: 'Faltam Campos a serem preenchidos!'});
+        }
+
+        const usuarioExistente = await userModel.buscarUsuarioPorEmail(email);
+        if (usuarioExistente){
+            return res.status(400).send({error: 'Email já cadastrado!'});
+        }
+
+        await userModel.criarUsuario({nome_usuario, email, senha, tipo});
+        res.status(201).json({message: 'Usuário cadastrado com sucesso!'});
+    }
+    catch (error) {
+        res.status(500).json({error: "Erro ao registrar usuário!"});
+    }
+};
+
+const login = async (req, res) => {
+    try {
+      const { email, senha } = req.body;
+  
+      const usuario = await userModel.buscarUsuarioPorEmail(email);
+      if (!usuario) {
+        return res.status(400).send({ error: 'Usuário não encontrado!' });
+      }
+  
+      const senhaValida = await bcrypt.compare(senha, usuario.senha);
+      if (!senhaValida) {
+        return res.status(401).send({ error: 'Senha inválida!' });
+      }
+  
+      const token = jwt.sign(
+        { id: usuario.id, tipo: usuario.tipo },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      );
+  
+      //retornando dados extras além do token pra mostrar na home
+      res.json({
+        token,
+        usuarioId: usuario.id,
+        nome_usuario: usuario.nome_usuario,
+        tipo: usuario.tipo
+      });
+    } catch (error) {
+        
+      console.error('Erro ao fazer login:', error);
+      res.status(500).json({ error: "Erro ao fazer login!" });
+    }
+  };
+
+module.exports = {
+    register, 
+    login
+};
