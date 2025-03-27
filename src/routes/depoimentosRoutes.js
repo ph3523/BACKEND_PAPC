188cbe0a06aa63e61d3390daa68f24eeaf27fa17
EldFranklin <8wesleyfranklin@gmail.com>
2025-03-27 17:28:49 -0300

    depoimentos com autenticação corrigido

--- src/routes/depoimentosRoutes.js
+++ src/routes/depoimentosRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const depoimentosController = require('../controllers/depoimentosController');
+const authMiddleware = require('../middleware/authMiddleware'); //importa pra projeter as rotas
 
 router.get('/', depoimentosController.listarDepoimentos);
 router.get('/:id', depoimentosController.buscarDepoimentoPorId);
-router.post('/', depoimentosController.criarDepoimento);
+
+// criação de depoimento protegida por autenticação
+router.post('/', authMiddleware, depoimentosController.criarDepoimento);
+
 router.put('/:id', depoimentosController.atualizarDepoimento);
 router.delete('/:id', depoimentosController.excluirDepoimento);
 
